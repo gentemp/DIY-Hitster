4a1af04ef3979b7a66826ddb90df5b674fa3f57e
Johan Lindberg <johan@expan.se>
2024-10-02 08:26:13 +0200

    Removed instructions from template-file.

--- credentials/template_spotify.py
+++ credentials/template_spotify.py
@@ -1,6 +1,5 @@
-# Go to https://developer.spotify.com/ and create an app to get started.
-# Once you've got one, paste your client id and secret here to use with the scripts.
-# Make sure to rename the file from template_spotify.py to spotify.py before use.
+# this file keeps your credentials for a Spotify app.
+# See https://developer.spotify.com/ to get one.
 
 CLIENT_ID = '<insert your id here>'
 CLIENT_SECRET = '<insert your secret here>'
