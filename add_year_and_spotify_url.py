28ddcc9688d070053b563df77a7e582fe380d8ba
Johan Lindberg <johan@expan.se>
2024-11-05 21:21:56 +0100

    Refactoring. Moved out common functions to tools.py

--- add_year_and_spotify_url.py
+++ add_year_and_spotify_url.py
@@ -1,47 +1,16 @@
 # helper script to add year and spotify-url
 
-import requests
-
-from functools import cache
-from credentials.spotify import CLIENT_ID, CLIENT_SECRET
-
-BASE_URL = 'https://api.spotify.com/v1/'
-AUTH_URL = 'https://accounts.spotify.com/api/token'
-
-def save_contents(filename, contents):
-    with open(filename, 'w') as _file:
-        _file.write(contents)
-
-def get_contents(filename):
-    with open(filename, 'r') as _file:
-        contents = _file.readlines()
-    return contents
-
-@cache
-def get_access_token(id, secret):
-    auth_response = requests.post(AUTH_URL,
-    {   
-        'grant_type': 'client_credentials',
-        'client_id': id,
-        'client_secret': secret
-    })
-    auth_response_data = auth_response.json()
-    return auth_response_data['access_token']
-
-def _spotify_request(request):
-    headers = { 'Authorization':
-                'Bearer {token}'.format(token = get_access_token(CLIENT_ID, CLIENT_SECRET)) }
-    return requests.get(request, headers = headers).json()
+from tools import *
 
 def get_id_and_url(artist, track, market):
-    result = _spotify_request(BASE_URL + 'search/?q=artist:' + artist + \
+    result = spotify_request(BASE_URL + 'search/?q=artist:' + artist + \
             ' track:' + track + '&type=track&market=' + market + '&limit=1')    
     return result['tracks']['items'][0]['id'], result['tracks']['items'][0]['external_urls']['spotify'] + '?si='
 
 def get_year(track_id):
     # This is not neccessarily the year that you want! Make sure to
     # double check before you publish your PDF
-    result = _spotify_request(BASE_URL + 'tracks/' + track_id)
+    result = spotify_request(BASE_URL + 'tracks/' + track_id)
     return result['album']['release_date'][:4]
 
 def fix(contents, market):
--- csv_from_spotify_playlist.py
+++ csv_from_spotify_playlist.py
@@ -1,36 +1,10 @@
-# helper script to add year and spotify-url
+# helper script to create a csv file from a spotify playlist
 
-import requests
-
-from functools import cache
-from credentials.spotify import CLIENT_ID, CLIENT_SECRET
-
-BASE_URL = 'https://api.spotify.com/v1/'
-AUTH_URL = 'https://accounts.spotify.com/api/token'
-
-def save_contents(filename, contents):
-    with open(filename, 'w') as _file:
-        _file.write(contents)
-
-@cache
-def get_access_token(id, secret):
-    auth_response = requests.post(AUTH_URL,
-    {   
-        'grant_type': 'client_credentials',
-        'client_id': id,
-        'client_secret': secret
-    })
-    auth_response_data = auth_response.json()
-    return auth_response_data['access_token']
-
-def _spotify_request(request):
-    headers = { 'Authorization':
-                'Bearer {token}'.format(token = get_access_token(CLIENT_ID, CLIENT_SECRET)) }
-    return requests.get(request, headers = headers).json()
+from tools import *
 
 def get_data(track_id):
     result = ''
-    data = _spotify_request(BASE_URL + 'tracks/' + track_id)
+    data = spotify_request(BASE_URL + 'tracks/' + track_id)
     result += data['album']['release_date'][:4] + ';'
     result += ', '.join([artist['name'] for artist in data['artists']]) + ';'
     result += data['name'] + ';'
@@ -40,14 +14,14 @@ def get_data(track_id):
 
 def get_playlist_contents(playlist):
     result = []
-    tracks = _spotify_request(BASE_URL + 'playlists/' + playlist + '/tracks')
+    tracks = spotify_request(BASE_URL + 'playlists/' + playlist + '/tracks')
     while True: 
         for item in tracks['items']:
             track_id = item['track']['id']
             result.append(get_data(track_id))
         
         if tracks['next']:
-            tracks = _spotify_request(tracks['next'])
+            tracks = spotify_request(tracks['next'])
         else:
             break
 
