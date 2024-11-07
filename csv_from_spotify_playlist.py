c19ff9c456e06b8c97aa0f130d0d177ec899508c
Johan Lindberg <johan@expan.se>
2024-11-07 08:23:14 +0100

    Added code to only query Spotify if the song isn't already in the csv
    The benefit of this is that if you make changes to, for example, the year it
    isn't overwritten by the Spotify web API data. Also, the script runs a lot
    quicker if you build the csv incrementally.

--- csv_from_spotify_playlist.py
+++ csv_from_spotify_playlist.py
@@ -12,13 +12,25 @@ def get_data(track_id):
 
     return result
 
-def get_playlist_contents(playlist):
+def get_playlist_contents(playlist, backup):
+    _backup_dict = {}
+    if backup:
+        for line in backup:
+            _, _, _, url = line.split(';')
+            # extract the id part from the url
+            # https://open.spotify.com/track/ 28ihA2Fp5zRmcmi8hpfWiC ?si=
+            id = url[31:-5]
+            _backup_dict[id] = line.strip()
+
     result = []
     tracks = spotify_request(BASE_URL + 'playlists/' + playlist + '/tracks')
     while True: 
         for item in tracks['items']:
             track_id = item['track']['id']
-            result.append(get_data(track_id))
+            try:
+                result.append(_backup_dict[track_id])
+            except KeyError:
+                result.append(get_data(track_id))
         
         if tracks['next']:
             tracks = spotify_request(tracks['next'])
@@ -36,5 +48,12 @@ if __name__ == "__main__":
     parser.add_argument('playlist')
     args = parser.parse_args()
 
-    contents = get_playlist_contents(args.playlist)
+    # reuse existing rows in order to both speed up the script execution
+    # and to keep manually fixed values
+    backup = None
+    try:
+        backup = get_contents(args.filename)
+    except FileNotFoundError:
+        pass
+    contents = get_playlist_contents(args.playlist, backup)
     save_contents(args.filename, '\n'.join(contents))
